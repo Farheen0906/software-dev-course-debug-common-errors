8156054fd6faa25b7fae857d7f47dc4774335a67
Farheen <farhashaik99@gmail.com>
2025-09-04 19:50:55 -0400

    added the error messages

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,29 +24,38 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+//console.log("Welcome to the bootcamp
 
-// What’s Wrong?
-
+//Correct code
+console.log("Welcome to the bootcamp");
+/*Syntax error : by reading the error message it is clear that,
+ it is missing closing quotation marks and parantheses and semicolon.
+*/
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
-
+/*
 let numbers = [2, 4, "eight"];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
+*/
 
-// What’s Wrong?
+//Runtime error - as "eight" is a string when we multiply with 2 the result is NaN 
+//solution - creating an array with only numbers should work perfectly
 
-
+let numbers = [2, 4, "10"]; // directly a number could be added or number as string type can also be added
+for (let i = 0; i < numbers.length; i++) {
+  let doubled = numbers[i] * 2; // implicit conversion of string to number type before the multiplication 
+  console.log(doubled);
+}
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
-
+/*
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
@@ -56,7 +65,23 @@ function isPrime(num) {
   }
   return false; // Supposed to indicate num IS prime
 }
-
 console.log(isPrime(7)); // Expected true but gets false
+*/
+// Logic Error - as wrong return boolean values were assigned to the logic
+// Updated code
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;  // Supposed to indicate num is NOT prime
+      //If the reminder after division with 2 is 0 it indicates the given number is prime 
+      //so it should return true but not false
 
-// What’s Wrong?
+    }
+  }
+  return true; // Supposed to indicate num IS prime
+
+}
+console.log(isPrime(7)); // Expected true but gets false
+console.log(isPrime(11));// return true as 11 is a prime number
+console.log(isPrime(4)); // returns false
